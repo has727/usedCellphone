f8291020572db9eed0a728742fdef9662bb5e3de
has727 <has727@outlook.com>
2017-08-28 08:53:28 +0800

    1. Registered models in 'admin.py'

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from mysite.models import Maker, Product, PModel, PPhoto
 
 # Register your models here.
+
+admin.site.register(Maker)
+admin.site.register(PPhoto)
+admin.site.register(Product)
+admin.site.register(PModel)
